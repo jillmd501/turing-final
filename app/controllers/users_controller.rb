18ba20caa7209534eed92de3f52d0da459691aeb
Jill Donohue <jillmd501@gmail.com>
2016-03-08 13:38:46 -0700

    user can addlink

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to @user
+      redirect_to links_path
     else
       flash.now[:error] = @user.errors.full_messages.join(', ')
       render :new
